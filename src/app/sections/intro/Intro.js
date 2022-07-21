7d54effce532b1535083936099a62a2154d15980
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-07-21 21:30:54 +0100

    Update Intro.js
    added my portrait to the section

--- src/app/sections/intro/Intro.js
+++ src/app/sections/intro/Intro.js
@@ -1,5 +1,6 @@
 import React, { useRef } from "react";
 import { useIntersection } from "../../useIntersection";
+import PhotoOfMe from "../../../images/me.jpg";
 import "./Intro.scss";
 
 export const Intro = () => {
@@ -22,15 +23,16 @@ export const Intro = () => {
   }
 
   return (
-    <section>
+    <section id="intro">
       <div ref={ref} className="intro-container hidden">
-        <div className="left-aligned-text text-block">
+        <div className="portrait-container">
+          <img id="image-me" src={PhotoOfMe} alt="a photo of me"></img>
+        </div>
+        <div className="intro-text-contanier">
           <p className="preHeading">My name is</p>
           <h1 className="headline mainHeading">Jason Smith</h1>
-          <h2 className="headline">
-            I'm an aspiring{" "}
-            <span className="white-text">front-end developer</span>
-          </h2>
+          <p className="preHeading">I'm an aspiring</p>
+          <h2 className="headline white-text">front-end developer</h2>
           <p>
             I'm <span id="myAge">{calculateAge()}</span> years old and I'm a
             super keen coder whose aim is to continue progressing my skills and
